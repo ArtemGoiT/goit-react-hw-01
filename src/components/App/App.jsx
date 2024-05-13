6ff76d54fbd62c34280cf0f99b97b06211e8153b
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-05-13 12:55:16 +0200

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,17 @@
 import './App.css';
 import Profile from '../Profile/Profile';
 import FriendList from '../FriendList/FriendList';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
 
 import userData from '../../userData.json';
 import friends from '../../friends.json';
+import transactions from '../../transactions.json';
 const App = () => {
   return (
     <div className="container">
       <Profile {...userData} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
